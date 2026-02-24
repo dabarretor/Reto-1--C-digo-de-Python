7ec424e4c863ea10cada74dba0fb5aa2b51fedd4
Daniel_Barregoat <dabarretor@unal.edu.co>
2026-02-23 21:32:00 -0500

    Refactor palindrome checker with enhanced comments
    Updated the palindrome checking program with improved comments and user prompts.

--- punto2.py
+++ punto2.py
@@ -1,12 +1,20 @@
-palindromo=str(input("digite una palabra: "))
-palindromo = palindromo.lower().replace(" ", "")
+""" This program checks if a word is a palindrome or not. 
+A palindrome is a word that reads the same backward as forward,
+ignoring spaces and accents."""
+
+palindrome = str(input("write a word: "))
+# It allows you to remove spaces if the word contains them.
+palindrome = palindrome.lower().replace(" ", "")
 i = 0
-j = len(palindromo) - 1
-while i<j:
-    if palindromo[i]!=palindromo[j]:
-        print("la palabra no es un palindromo: ", palindromo)
+# Returns the text string to a number to recognize the last position.
+j = len(palindrome) - 1 
+
+while i < j:
+    # If the position of the letters is different, it is not a palindrome.
+    if palindrome[i] != palindrome[j]: 
+        print(f"The word is not a palindrome: {palindrome}")
         break
-    i +=1
-    j -=1
+    i += 1 # Increases the position if the letters are the same.
+    j -= 1 # Decreases the position if the letters are the same.
 else:
-    print("la palabra es un palindromo: ", palindromo)
+    print(f"The word is a palindrome: {palindrome}")
