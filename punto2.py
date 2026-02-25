a4ce5c0baf6cba6b3f598a353d55fac6a737ba7d
Daniel_Barregoat <dabarretor@unal.edu.co>
2026-02-25 15:54:17 -0500

    Refactor palindrome check into a function

--- punto2.py
+++ punto2.py
@@ -1,20 +1,27 @@
-""" This program checks if a word is a palindrome or not. 
+""" This program checks if a word is a palindrome or not.
 A palindrome is a word that reads the same backward as forward,
 ignoring spaces and accents."""
 
-palindrome = str(input("write a word: "))
-# It allows you to remove spaces if the word contains them.
-palindrome = palindrome.lower().replace(" ", "")
-i = 0
-# Returns the text string to a number to recognize the last position.
-j = len(palindrome) - 1 
+def is_palindrome(word):
+    # It allows you to remove spaces if the word contains them.
+    palindrome = word.lower().replace(" ", "")
+    left_index = 0
+    # Returns the text string to a number to recognize the last position.
+    right_index = len(palindrome) - 1
 
-while i < j:
-    # If the position of the letters is different, it is not a palindrome.
-    if palindrome[i] != palindrome[j]: 
-        print(f"The word is not a palindrome: {palindrome}")
-        break
-    i += 1 # Increases the position if the letters are the same.
-    j -= 1 # Decreases the position if the letters are the same.
+    while left_index < right_index:
+        # If the position of the letters is different, it is not a palindrome.
+        if palindrome[left_index] != palindrome[right_index]:
+            return False
+        left_index += 1 # Increases the position if the letters are the same.
+        right_index -= 1 # Decreases the position if the letters are the same.
+
+    return True
+
+
+word = str(input("write a word: "))
+
+if is_palindrome(word):
+    print(f"The word is a palindrome: {word}")
 else:
-    print(f"The word is a palindrome: {palindrome}")
+    print(f"The word is not a palindrome: {word}")
